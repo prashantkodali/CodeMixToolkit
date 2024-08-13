8f3db272b50fe4772c9fd90140127957625f6ab9
Ashna Dua <ashnadua220011@gmail.com>
2024-08-13 12:13:56 +0530

    word-noise.py

--- word-noise.py
+++ word-noise.py
@@ -1,3 +1,9 @@
+from datasets import load_from_disk, Dataset, DatasetDict
+import random
+import pandas as pd
+
+original_dataset = load_from_disk("dataset")
+
 def eliminate_vowels(word):
     vowels = 'aeiou'
     return ''.join(c for c in word if c not in vowels)
@@ -16,7 +22,36 @@ def generate_noisy_form(word, rule):
         noisy_word = remove_duplicate_consonants(word)
     return noisy_word
 
-input_word = input()
-rule = int(input())
-noisy_form = generate_noisy_form(input_word, rule)
-print(noisy_form)
+noisy_datasets = {}
+
+for split in ["train", "test", "validation"]:
+    noisy_samples = []
+
+    split_df = original_dataset[split].to_pandas()
+
+    for index, row in split_df.iterrows():
+        word = row["CM_candidates_transliterated_indictrans"]
+        sentence_length = len(word)
+        num_characters_to_modify = max(1, sentence_length // 10)
+
+        positions_to_modify = random.sample(range(sentence_length), num_characters_to_modify)
+        noisy_word = list(word)
+        for pos in positions_to_modify:
+            if pos < sentence_length - 1 and word[pos] not in 'aeiou' and word[pos+1] not in 'aeiou':
+                random_rule = 2
+            else:
+                random_rule = random.choice([1, 2])
+            noisy_word[pos] = generate_noisy_form(word[pos], random_rule)
+        noisy_word = ''.join(noisy_word)
+        noisy_example = {key: row[key] for key in row.keys()}
+        noisy_example["noisy_data"] = noisy_word
+        noisy_samples.append(noisy_example)
+
+    noisy_datasets[split] = Dataset.from_pandas(pd.DataFrame(noisy_samples))
+
+noisy_data = DatasetDict({
+    "train": noisy_datasets["train"],
+    "validation": noisy_datasets["validation"],
+    "test": noisy_datasets["test"]})
+
+noisy_data.save_to_disk("noisy_dataset")
