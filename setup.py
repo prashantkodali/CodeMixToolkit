f4f3164cde0f029a415d9fb579a3175b58e55cb5
Prashant Kodali <kodali.prashant@gmail.com>
2025-05-17 10:58:52 +0530

    setup.py: add requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ def read_version():
     return version["__version__"]
 
 
+def read_requirements():
+    with open("requirements.txt") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
 setup(
     name="codemixtoolkit",
     version=read_version(),
@@ -16,27 +21,7 @@ setup(
     description="A toolkit for code-mixed language processing",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[
-        "ai4bharat_transliteration==1.1.3",
-        "alphabet_detector==0.0.7",
-        "datasets==3.3.1",
-        "htbuilder==0.9.0",
-        "indic_nlp_library==0.92",
-        "ipython==8.12.3",
-        "litellm==1.69.1",
-        "numpy==2.2.5",
-        "pandas==2.2.3",
-        "python-dotenv==1.1.0",
-        "PyYAML==6.0.2",
-        "sacrebleu==2.5.1",
-        "scikit_learn==1.6.1",
-        "setuptools==75.8.0",
-        "st_annotated_text==4.0.2",
-        "stanza==1.10.1",
-        "torch==2.4.0",
-        "tqdm==4.67.1",
-        "transformers==4.49.0",
-    ],
+    install_requires=read_requirements(),
 )
 
 # [
